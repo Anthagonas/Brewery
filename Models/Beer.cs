f2e3b168ff058680750b59aef94f81bcce9250d1
Anthagonas <26145060+Anthagonas@users.noreply.github.com>
2022-10-03 11:05:58 +0200

    refactor Beer to contain brewery data

--- Models/Beer.cs
+++ Models/Beer.cs
@@ -2,9 +2,16 @@ namespace BeerManager.Models
 {
     public class Beer
     {
+        #region Beer
         public string Id { get; set; }
         public string Name { get; set; }
         public double AlcoholContent { get; set; }
         public double Price { get; set; }
+        #endregion
+
+        #region Brewery
+        public string BreweryId { get; set; }
+        public string BreweryName { get; set; }
+        #endregion
     }
 }
