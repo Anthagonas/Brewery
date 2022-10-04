2873494c610759c31a5bc556e5e59d05b5df34d2
Anthagonas <26145060+Anthagonas@users.noreply.github.com>
2022-10-04 20:16:04 +0200

    Added Empty WholesalerController

---
+++ Controllers/WholesalerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BeerManager.Controllers
+{
+    [Route("wholesaler")]
+    [ApiController]
+    public class WholesalerController : ControllerBase
+    {
+        // PATCH wholesaler/wholesalerId/beerId
+        [HttpPatch("{wholesalerId}")]
+        public void ModifyBeerStock(string wholesalerId, string beerId, [FromBody] string newStockAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST wholesaler/wholesalerId
+        [HttpPost("{wholesalerId}")]
+        public void AddBeer(string wholesalerId, [FromBody] string newBeerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
