b6b9e638955575d4cccb3595ec70f18cd64ffdc9
Anthagonas <26145060+Anthagonas@users.noreply.github.com>
2022-10-04 20:36:31 +0200

    Added dummy dataset

--- Controllers/WholesalerController.cs
+++ Controllers/WholesalerController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BeerManager.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 namespace BeerManager.Controllers
 {
@@ -7,6 +9,17 @@ namespace BeerManager.Controllers
     [ApiController]
     public class WholesalerController : ControllerBase
     {
+        #region DataSet
+        List<Wholesaler> beers = new List<Wholesaler>{
+                    new Wholesaler
+                    {
+                        Id = "777777777",
+                        Name = "A wholesaler",
+                        Stock = new Dictionary<string, int>{ { "987654321", 10 } }
+                    }
+        };
+        #endregion
+
         // PATCH wholesaler/wholesalerId/beerId
         [HttpPatch("{wholesalerId}")]
         public void ModifyBeerStock(string wholesalerId, string beerId, [FromBody] string newStockAmount)
