dfa14bb51eb67447d58ba5d135655f49ca63a832
Anthagonas <26145060+Anthagonas@users.noreply.github.com>
2022-10-07 19:08:25 +0200

    Added request quote route

--- Controllers/WholesalerController.cs
+++ Controllers/WholesalerController.cs
@@ -41,7 +41,7 @@ namespace BeerManager.Controllers
         [HttpPost("{wholesalerId}")]
         public IActionResult AddBeer(string wholesalerId, [FromBody] Dictionary<string,int> newBeerStock)
         {
-            if (!newBeerStock?.Any() ?? true || newBeerStock.Keys.Count > 1)
+            if (newBeerStock is null || !newBeerStock.Any() || newBeerStock.Keys.Count > 1)
             {
                 return BadRequest("Invalid beer count");
             }
@@ -60,6 +60,54 @@ namespace BeerManager.Controllers
             }
 
             return CreatedAtAction(null, newBeer.Key);
+        }
+
+        //TODO : A client can request a quote from a wholesaler
+        /*
+         * 1- If successful, the method returns a price and a summary of the quote.
+                o A 10% discount is applied above 10 drinks
+                o A 20% discount is applied above 20 drinks
+         * 2- If there is an error, it returns an exception and a message to explain the reason:
+                o The order cannot be empty
+                o The wholesaler must exist
+                o There can't be any duplicate in the order
+                o The number of beers ordered cannot be greater than the wholesaler's stock
+                o The beer must be sold by the wholesaler
+         */
+        // POST wholesaler/wholesalerId/quote
+        [HttpPost("{wholesalerId}/quote")]
+        public IActionResult RequestQuote(string wholesalerId, [FromBody] Dictionary<string, int> order)
+        {
+            if (order is null || !order.Any())
+            {
+                return BadRequest("The order cannot be empty");
+            }
+            //TODO : Check duplicate beerId in order
+            //return BadRequest("There can't be any duplicate in the order");
+
+            QuoteSummary quoteSummary = new QuoteSummary();
+            foreach (KeyValuePair<string,int> beer in order)
+            {
+                string beerId = beer.Key;
+                int amountOfBeerOrdered = beer.Value;
+
+                int availableStock = _wholesalerRepository.GetBeerStockAmount(wholesalerId, beerId);
+                if(availableStock < amountOfBeerOrdered)
+                {
+                    return BadRequest("The number of beers ordered cannot be greater than the wholesaler's stock");
+                } 
+                else if (amountOfBeerOrdered < 1)
+                {
+                    return BadRequest("The number of beers ordered cannot be lower than 1");
+                }
+                else
+                {
+                    double beerUnitPrice = _beerRepository.GetBeerPrice(beerId);
+                    quoteSummary.AddNewElement(beerId, amountOfBeerOrdered, beerUnitPrice);
+                }
+            }
+
+            return Ok(quoteSummary);
         }
     }
 }
