da75b3797965a55b47a4e9ff351d12d5a7555407
Anthagonas <26145060+Anthagonas@users.noreply.github.com>
2022-10-07 19:07:57 +0200

    Added QuoteSummary class to format quote output

---
+++ Models/QuoteSummary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeerManager.Models
+{
+    public class QuoteSummary
+    {
+        public double TotalPrice { get; set; }
+        public int TotalItems { get; set; }
+        public List<QuoteDetail> Details { get; set; }
+
+        public void AddNewElement(string name, int amount, double unitPrice)
+        {
+            TotalItems += amount;
+            Details.Add(new QuoteDetail(name, amount, unitPrice));
+            CalculateNewTotalPrice();
+        }
+
+        private void CalculateNewTotalPrice()
+        {
+            TotalPrice = Details.Sum(line => line.Price);
+            if(TotalItems > 20)
+            {
+                TotalPrice = CalculateDiscount(TotalPrice, 20);
+            }
+            else if (TotalItems > 10)
+            {
+                TotalPrice = CalculateDiscount(TotalPrice, 10);
+            }
+        }
+
+        private double CalculateDiscount(double initialPrice, double percentage)
+        {
+            return initialPrice - (initialPrice * percentage / 100);
+        }
+
+        public class QuoteDetail
+        {
+            public string Name { get; set; }
+            public int Amount { get; set; }
+            public double UnitPrice { get; set; }
+            public double Price { get; set; }
+            public QuoteDetail(string name, int amount, double unitPrice)
+            {
+                Name = name;
+                Amount = amount;
+                UnitPrice = unitPrice;
+                Price = unitPrice * amount;
+            }
+        }
+    }
+}
